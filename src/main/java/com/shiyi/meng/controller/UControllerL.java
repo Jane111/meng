e5451fb9d704000a2978ed0111ab685a79765fa0
liujane <1171603265@qq.com>
2019-09-14 16:11:02 +0800

    0416刘静

--- src/main/java/com/shiyi/meng/controller/UControllerL.java
+++ src/main/java/com/shiyi/meng/controller/UControllerL.java
@@ -1,10 +1,17 @@
 package com.shiyi.meng.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.shiyi.meng.service.UServiceL;
 import com.shiyi.meng.util.BaseResponse;
+import com.shiyi.meng.util.Constant;
+import com.shiyi.meng.util.HttpClientUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -13,4 +20,56 @@ public class UControllerL {
     UServiceL uServiceL;
     @Autowired
     BaseResponse jr;
+    //1、小程序用户授权
+    @RequestMapping("/authorize")
+    public BaseResponse authorize(
+            @RequestParam(value="code",required = false) String code)
+    {
+        // 配置请求参数
+        Map<String, String> param = new HashMap<>();
+        param.put("appid", Constant.APPID);
+        param.put("secret", Constant.APPSECRET);
+        param.put("js_code", code);
+        param.put("grant_type", Constant.GRANTTYPE);
+        // 发送请求
+        System.out.println("code="+code);
+        String wxResult = HttpClientUtil.doGet(Constant.LOGINURL, param);
+        System.out.println(wxResult);
+        JSONObject jsonObject = JSONObject.parseObject(wxResult);
+        // 获取参数返回的
+        String session_key = jsonObject.get("session_key").toString();
+        String open_id = jsonObject.get("openid").toString();
+        // 封装返回小程序
+        Map<String, String> result = new HashMap<>();
+        result.put("session_key", session_key);
+        result.put("open_id", open_id);
+        // 根据返回的user实体类，判断用户是否是新用户，是的话，将用户信息存到数据库
+//        Visitor vs = bs.selectByOpenId(open_id);
+//        if(vs == null){
+//
+//            Visitor insert_visitor = new Visitor();
+//            insert_visitor.setVOpenId(open_id);
+//
+//            // 添加到数据库
+//            Boolean flag = bs.insertVisitor(insert_visitor);
+//            if(!flag)
+//            {
+//                jr.setResult(ResultCodeEnum.ADD_ERROR);
+//            }
+//        }
+//        else{
+//            Business business = Business.dao.findFirst("select bId from business where bOpenId=?",open_id);
+//            if(business==null)//游客身份
+//            {
+//                result.put("bId","");
+//            }
+//            else//business身份
+//            {
+//                result.put("bId", business.getBId()+"");
+//            }
+//            jr.setResult(ResultCodeEnum.SUCCESS);
+//        }
+//        jr.setData(result);
+        return jr;
+    }
 }
---
+++ src/main/java/com/shiyi/meng/util/Constant.java
@@ -0,0 +1,22 @@
+package com.shiyi.meng.util;
+
+import java.math.BigInteger;
+
+public class Constant {
+
+	//小程序相关信息
+	public static final String LOGINURL="https://api.weixin.qq.com/sns/jscode2session";
+	public static final String APPID="wxe3d360d7f680f247";
+	public static final String APPSECRET="3fa1771b7860267bbcc74c15e6b2044e";
+	public static final String GRANTTYPE="authorization_code";
+
+	//图像存储相关信息
+//	public static  final String Secret_id="AKID0jJtXvZOlMn7RVnncuQtJn1zgOyIHFWK";
+//	public static final String Secret_key="FzUgdXUmobAwSsIWtQG8l8HI3cy3A4jC";
+//    public static final String PASSWORD = "123456";
+//	public static final String PATH = "E:/IDEAworkspace/cross2u/Cross2U/blockchain/keystore/UTC--2019-03-19T03-20-23.563424700Z--b3bc658eeee4972b29596e3441e9cfbab06fc651";
+//
+//	public static final String ADDRESS = "http://localhost:9000";
+//	public static final BigInteger GAS_PRICE = BigInteger.valueOf(1);
+//	public static final BigInteger GAS_LIMIT =BigInteger.valueOf(41000) ;
+}
